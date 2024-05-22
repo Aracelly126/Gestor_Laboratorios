d2a669e1c94a424b68b8f3f418d037e7a28e8772
Christian Hurtado <tcriss002@gmail.com>
2024-05-21 21:46:50 -0500

    Se creo la ventana de reservas

--- VanguardiaProject/src/Utils/Conex.java
+++ VanguardiaProject/src/Utils/Conex.java
@@ -6,7 +6,7 @@ import javax.swing.JOptionPane;
 public class Conex {
     
     private static int puerto = 3415;
-    private static String url = "jdbc:mysql://localhost:" + puerto + "/";
+    private static String url = "jdbc:mysql://localhost:" + 3306 + "/";
     private static String nombre_db = "Vanguardia";
     private static String user = "root";
     private static String pass = "";
