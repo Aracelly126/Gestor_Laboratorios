b15086c67250ebc3c69146de659b4ddef761dc0c
Mr-Dark65 <steevenloor2412@gmail.com>
2024-06-24 08:51:59 -0500

    Se implemento metodo para cargar datos a la tabla junto con el identificador unico, ademas, se realizo la logica para asignar un identificar auto-incrementable

--- VanguardiaProject/src/Windows/Profesores.java
+++ VanguardiaProject/src/Windows/Profesores.java
@@ -27,7 +27,7 @@ public class Profesores extends javax.swing.JPanel {
     Crud_AgregarProfesor ps = new Crud_AgregarProfesor();
     crud_editarProfesor ep = new crud_editarProfesor();
 
-    DefaultTableModel tablaModelo = new DefaultTableModel(new Object[]{"Profesor", "Materia", "Tipo", "Nombre", "Bloque"}, 0);
+    DefaultTableModel tablaModelo = new DefaultTableModel(new Object[]{"Identificador", "Cedula", "Nombre", "Apellido"}, 0);
     String nombre = "";
     int idpro = 0;
 
@@ -37,9 +37,9 @@ public class Profesores extends javax.swing.JPanel {
     public Profesores() {
         initComponents();
         jTable1.setModel(tablaModelo);
-        profesores.cargarBloque(jcbxBloques);
         profesores.cargarTabla(jTable1);
-        setEventHandlers();
+        jtxtIdentificador.setEnabled(false);
+        profesores.actualizarLabelIdentificador(jtxtIdentificador);
 
         jtxtNombre.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusLost(java.awt.event.FocusEvent evt) {
@@ -49,55 +49,10 @@ public class Profesores extends javax.swing.JPanel {
 
     }
 
-    private void setEventHandlers() {
-        jcbxBloques.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println(jcbxBloques.getSelectedItem().toString());
-                cargarAulasOLaboratorios();
-                //profesores.cargarMaterias(jcbxMateria, jcbxBloques, jcbxTipo);
-                
-                System.out.println(jcbxBloques.getSelectedItem().toString());
-            }
-        });
+    
 
-        jcbxTipo.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cargarAulasOLaboratorios();
-                cargarMateria();
-                
-                
-            }
-        });
-
-        jcbxMateria.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cargarMateria();
-            }
-        });
-    }
-
-    private void cargarMateria() {
-        String selectedBloque = (String) jcbxBloques.getSelectedItem();
-        String selectedTipo = (String) jcbxTipo.getSelectedItem();
-        profesores.cargarMaterias(jcbxMateria, selectedBloque, selectedTipo);
-    }
-
-    private void cargarAulasOLaboratorios() {
-        Object selectedBloqueObj = jcbxBloques.getSelectedItem();
-        Object selectedTipoObj = jcbxTipo.getSelectedItem();
-        if (selectedBloqueObj != null && selectedTipoObj != null) {
-            String selectedBloque = selectedBloqueObj.toString();
-            String selectedTipo = selectedTipoObj.toString();
-            System.out.println("Selected Bloque: " + selectedBloque);
-            System.out.println("Selected Tipo: " + selectedTipo);
-            profesores.llenarComboBoxAulas(jComboBox1, jcbxTipo, jcbxBloques);
-        } else {
-            System.out.println("No hay bloque o tipo seleccionado.");
-        }
-    }
+   
+   
 
     private void jtxtNombreFocusLost(java.awt.event.FocusEvent evt) {
         // Recuperar el texto del JTextField cuando pierde el foco
@@ -119,10 +74,7 @@ public class Profesores extends javax.swing.JPanel {
 
             // Mostrar los datos en los componentes correspondientes
             jtxtNombre.setText(nombre);
-            jcbxMateria.setSelectedItem(materiaSeleccionada);
-            jcbxTipo.setSelectedItem(tiposeleccionado);
-            jcbxBloques.setSelectedItem(bloqueSeleccionado);
-            jComboBox1.setSelectedItem(aulaSeleccionada);
+            
         } else {
             JOptionPane.showMessageDialog(null, "Por favor, seleccione una fila para editar.", "Aviso", JOptionPane.WARNING_MESSAGE);
         }
@@ -139,19 +91,18 @@ public class Profesores extends javax.swing.JPanel {
 
         jLabel1 = new javax.swing.JLabel();
         jtxtNombre = new javax.swing.JTextField();
-        jcbxMateria = new javax.swing.JComboBox<>();
-        jcbxBloques = new javax.swing.JComboBox<>();
-        jcbxTipo = new javax.swing.JComboBox<>();
         jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        jtxtApellido = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        jtxtCedula = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        jtxtIdentificador = new javax.swing.JTextField();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -159,31 +110,12 @@ public class Profesores extends javax.swing.JPanel {
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
         jLabel1.setText("PROFESORES");
         add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 30, -1, -1));
-        add(jtxtNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 130, 150, -1));
-
-        jcbxMateria.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { " " }));
-        add(jcbxMateria, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 180, 150, -1));
-
-        add(jcbxBloques, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 110, 140, -1));
-
-        jcbxTipo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "LABORATORIO", "AULA", "ESPECIAL" }));
-        jcbxTipo.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jcbxTipoActionPerformed(evt);
-            }
-        });
-        add(jcbxTipo, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 150, 140, -1));
+        add(jtxtNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 142, 150, -1));
 
         jLabel2.setText("Nombre:");
-        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 130, -1, -1));
+        add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 145, -1, -1));
 
-        jLabel3.setText("Bloque:");
-        add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 110, -1, -1));
-
-        jLabel4.setText("Tipo:");
-        add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 150, -1, -1));
-
-        jLabel5.setText("Materia:");
+        jLabel5.setText("Apellido:");
         add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 180, -1, -1));
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
@@ -234,29 +166,19 @@ public class Profesores extends javax.swing.JPanel {
             }
         });
         add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 220, -1, -1));
+        add(jtxtApellido, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 177, 150, -1));
 
-        add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 180, 140, -1));
+        jLabel6.setText("Cedula:");
+        add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 110, -1, -1));
+        add(jtxtCedula, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 107, 150, -1));
+
+        jLabel7.setText("Identificador:");
+        add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 110, -1, -1));
+        add(jtxtIdentificador, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 107, 80, -1));
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jcbxTipoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jcbxTipoActionPerformed
-        // TODO add your handling code here:
-        System.out.println(jtxtNombre.getText());
-
-    }//GEN-LAST:event_jcbxTipoActionPerformed
-
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        try {
-            String nombre = jtxtNombre.getText();
-            String materiaSeleccionada = (String) jcbxMateria.getSelectedItem();
-            String bloqueSeleccionado = (String) jcbxBloques.getSelectedItem();
-            String tipoSeleccionado = (String) jcbxTipo.getSelectedItem();
-            String aulaSeleccionada = (String) jComboBox1.getSelectedItem();
-            profesores.addProfesor(nombre, materiaSeleccionada, bloqueSeleccionado, tipoSeleccionado, aulaSeleccionada, jTable1);
-            jtxtNombre.setText("");
-            
-        } catch (Exception ex) {
-            Logger.getLogger(Profesores.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
@@ -271,8 +193,7 @@ public class Profesores extends javax.swing.JPanel {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         
-        profesores.editarProfesor(nombre, jcbxTipo.getSelectedItem().toString(), jComboBox1.getSelectedItem().toString(), jcbxMateria.getSelectedItem().toString(), nombre);
-
+        
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
@@ -289,17 +210,16 @@ public class Profesores extends javax.swing.JPanel {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
-    private javax.swing.JComboBox<String> jcbxBloques;
-    private javax.swing.JComboBox<String> jcbxMateria;
-    private javax.swing.JComboBox<String> jcbxTipo;
+    private javax.swing.JTextField jtxtApellido;
+    private javax.swing.JTextField jtxtCedula;
+    private javax.swing.JTextField jtxtIdentificador;
     private javax.swing.JTextField jtxtNombre;
     // End of variables declaration//GEN-END:variables
 }
